6a9487d22425484391ee7735e47ba3dfd3f489e8
My Thao Nguyen <mythng94@gmail.com>
2017-07-03 10:09:08 +0000

    Finish creating home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
+  
+  #ROOT, first page to show
+  root 'pages#home'
+  
+  # maps requests for the URL/pages/home to the about us action in the Pages controller.
+  # By using GET we arrange for the route to respond to a GET request.
+  # With this we generate a about us action inside the Pages controller, automatically
+  # get a page at the address /pages/about us
   get 'pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     
-    root 'application#hello'
+    
     
 end
