9c266a8c554874082a9648c2e33975975f5896e5
My Thao Nguyen <mythng94@gmail.com>
2017-07-05 09:55:14 +0000

    Finished user edit, update, index, destroy actions & admin rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   #before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:edit, :update]
-  #wrap_parameters :user, include: [:name, :email, :password, :password_confirmation]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   # GET /users/1
@@ -48,7 +51,7 @@ class UsersController < ApplicationController
     
     #Uses strong parameters to prevent mass assignment vulnerability
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "Profile was successfully updated."
       redirect_to @user
     else
       render 'edit'
@@ -58,8 +61,8 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    flash[:success] = "User was successfully destroyed."
+     User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
     redirect_to users_url
   end
 
@@ -74,11 +77,25 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    # BEFORE FILTERSs
+    
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+    
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
     
 # Returns the Gravatar for the given user. (The Profile img)
-  def gravatar_for(user, options = { size: 100 })
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")#"profile.png", alt: "profile picture", class:"gravatar")#gravatar_url, alt: user.name, class: "gravatar")
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
